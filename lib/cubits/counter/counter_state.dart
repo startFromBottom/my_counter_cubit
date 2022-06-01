b465190b398840ea5334e76d45717dacb4408df5
hyuem <uhh0701@gmail.com>
2022-06-01 22:10:51 +0900

    ch05. first commit & set cubit state style

---
+++ lib/cubits/counter/counter_state.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+part of 'counter_cubit.dart';
+
+class CounterState extends Equatable {
+  final int counter;
+  CounterState({
+    required this.counter,
+  });
+
+  factory CounterState.initial() {
+    return CounterState(counter: 0);
+  }
+
+  @override
+  // TODO: implement props
+  List<Object> get props => [counter];
+
+  CounterState copyWith({
+    int? counter,
+  }) {
+    return CounterState(
+      counter: counter ?? this.counter,
+    );
+  }
+
+  @override
+  bool get stringify => true;
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyCounter Cubit',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "0",
+          style: TextStyle(fontSize: 52.0),
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {},
+            heroTag: "increment",
+            child: Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: () {},
+            heroTag: "decrement",
+            child: Icon(Icons.remove),
+          )
+        ],
+      ),
+    );
+  }
+}
